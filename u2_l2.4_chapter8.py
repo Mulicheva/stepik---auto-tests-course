2fede662c7a50acce2a453e1e48bb626c2a45cc7
Katya Mulicheva <miokeit@gmail.com>
2020-02-29 19:44:12 +0400

    add file u2_l2.4_chapter8 :wq
     Please enter the commit message for your changes. Lines starting

---
+++ u2_l2.4_chapter8.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+browser = webdriver.Chrome("C:/chromedriver/chromedriver.exe")
+try:
+    browser.get(link)
+
+    elem = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+    )
+    button1 = browser.find_element_by_id("book").click()
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+    browser.execute_script("window.scrollBy(0, 100);")
+    input1 = browser.find_element_by_css_selector("#answer")
+    input1.send_keys(y)
+
+
+
+    time.sleep(1)
+    button1 = browser.find_element_by_id("solve").click()
+    #button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
